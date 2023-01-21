7aa936f8dbde1b531daf1b9f830deb6c6f847bd6
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-01-21 21:44:03 +0900

    Time: 9 ms (67.08%), Space: 5.8 MB (25.58%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.go
@@ -1,23 +1,16 @@
 func lengthOfLongestSubstring(s string) int {
     var result int
+    var start int
+    var temp string
+    
     for i := 0; i < len(s); i++ {
-        if (len(s) - i) <= result{
-            break
+        if strings.Contains(temp, string(s[i])) {
+            index := strings.IndexByte(temp, s[i])
+            start = start + index + 1
+            temp = temp[index+1:]
         }
-        if result < findSubStringLen(s, i){
-            result = findSubStringLen(s, i)
-        }  
+        result = int(math.Max(float64(result), float64(i - start + 1)))
+        temp = temp + string(s[i])
     }
     return result
-}
-
-func findSubStringLen(s string, start int) int {
-    var subString string
-    for i := start; i < len(s); i++ {
-        if strings.Contains(subString, string(s[i])) {
-            return len(subString)
-        }
-       subString = subString + string(s[i])
-    }
-    return len(subString)
 }
