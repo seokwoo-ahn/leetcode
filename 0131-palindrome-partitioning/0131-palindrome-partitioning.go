639d3a652ec254b947349788bd2ce6be1355a0c2
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-01-22 18:46:50 +0900

    Time: 415 ms (37.43%), Space: 28.9 MB (16.37%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.go
@@ -0,0 +1,43 @@
+func partition(s string) [][]string {
+    var index int
+    var substring string
+    var palindrome []string
+    var result [][]string
+    
+    findPalindrome(s, index, substring, palindrome, &result)
+    return result
+}
+
+func findPalindrome(s string, index int, substring string, palindrome []string, result *[][]string) {
+	if index == len(s) {
+		if isPalindrome(substring) && substring != "" {
+			tempPalindrome := make([]string, len(palindrome))
+			copy(tempPalindrome, palindrome)
+			tempPalindrome = append(tempPalindrome, substring)
+			*result = append(*result, tempPalindrome)
+		}
+		return
+	}
+
+	candidate := substring + string(s[index])
+	if isPalindrome(candidate) {
+		tempPalindrome := make([]string, len(palindrome))
+		copy(tempPalindrome, palindrome)
+		tempPalindrome = append(tempPalindrome, candidate)
+		findPalindrome(s, index+1, "", tempPalindrome, result)
+	}
+	findPalindrome(s, index+1, candidate, palindrome, result)
+}
+
+func isPalindrome(s string) bool {
+    start := 0
+    end := len(s) - 1
+    for start < end {
+        if s[start] != s[end]{
+            return false   
+        }
+        start++
+        end--
+    }
+    return true
+}
