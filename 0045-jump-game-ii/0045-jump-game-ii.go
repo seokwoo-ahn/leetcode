9c34a570156d9db0d8b536ffb76b161152027044
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-02-08 22:19:32 +0900

    Time: 4 ms (99.72%), Space: 5.9 MB (60.85%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.go
+++ 0045-jump-game-ii/0045-jump-game-ii.go
@@ -1,42 +1,18 @@
-type Jump struct {
-    position int
-    count int
-}
-
 func jump(nums []int) int {
-    if len(nums) < 2 {
-        return 0
-    }
-    init := Jump{}
+    max := 0
+    cur := 0
+    jumps := 0
     
-    check := make([]bool, len(nums))
-    var queue []Jump
-    
-    queue = append(queue, init)
-    
-    for len(queue) != 0 {
-        p := queue[0]
-        
-        queue = queue[1:]
-        
-        jump := nums[p.position]
-        if jump == 0 {
-            continue
+    for i:=0; i<len(nums)-1; i++ {
+        max = int(math.Max(float64(max), float64(i+nums[i])))
+        if cur == i {
+            jumps++
+            cur = max
         }
         
-        for i := 1; i < jump + 1; i++ {
-            var newPos Jump
-            newPos.position = p.position + i
-            if check[newPos.position] == true {
-                continue
-            }
-            newPos.count = p.count + 1
-            if newPos.position == len(nums) - 1{
-                return newPos.count
-            }
-            queue = append(queue, newPos)
-            check[newPos.position] = true
+        if cur > len(nums) -1 {
+            return jumps
         }
     }
-    return 0
+    return jumps
 }
