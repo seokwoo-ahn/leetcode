9256ff7ef76ee8da757502a6192a92c513c5eced
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-02-28 22:30:31 +0900

    Time: 3 ms (69.42%), Space: 2.1 MB (8.13%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.go
@@ -0,0 +1,17 @@
+func mySqrt(x int) int {
+    first := 0
+    last := x
+    
+    for first <= last {
+        mid := (first + last)/2
+        if mid*mid > x {
+            last = mid - 1
+        } else if mid*mid < x {
+            first = mid + 1
+        } else {
+            return mid
+        }
+    }
+    
+    return first - 1
+}
