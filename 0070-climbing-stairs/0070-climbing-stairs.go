40847f5f62b9b16436674731ec9ddeacc8a4994a
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-03-01 15:44:36 +0900

    Time: 2 ms (8.50%), Space: 2 MB (22.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.go
@@ -0,0 +1,15 @@
+func climbStairs(n int) int {
+    if n == 1 {return 1}
+    if n == 2 {return 2}
+    
+    arr := make([]int, n + 1)
+    
+    arr[1] = 1
+    arr[2] = 2
+    
+    for i := 3; i <= n; i++ {
+        arr[i] = arr[i-1] + arr[i-2]
+    }
+    
+    return arr[n]
+}
