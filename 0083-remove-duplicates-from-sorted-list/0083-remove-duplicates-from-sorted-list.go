d02e3ef3ac31cbfd0d9c11d63c8f914a3acc9fcf
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-03-04 23:10:24 +0900

    Time: 3 ms (72.89%), Space: 3.1 MB (19.95%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+    cur := head
+    for cur != nil {
+        if cur.Next != nil && cur.Val == cur.Next.Val{
+            cur.Next = cur.Next.Next
+        } else {
+             cur = cur.Next   
+        }
+    }
+    
+    return head
+}
