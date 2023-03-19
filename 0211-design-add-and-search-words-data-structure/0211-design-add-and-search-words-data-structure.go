2a9f54233888c3d06bb5c91fd65953da18bf1e90
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-03-19 10:37:18 +0900

    Time: 673 ms (81.92%), Space: 70.4 MB (33.90%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.go
@@ -0,0 +1,56 @@
+type WordDictionary struct {
+    letter [26]*WordDictionary
+    end bool
+}
+
+
+func Constructor() WordDictionary {
+    return WordDictionary{}
+}
+
+
+func (this *WordDictionary) AddWord(word string)  {
+    cur := this
+    for i := 0; i < len(word); i++ {
+        if cur.letter[word[i]- 'a'] == nil {
+             temp := Constructor()
+             cur.letter[word[i]- 'a'] = &temp   
+        }
+        cur = cur.letter[word[i] - 'a']
+    }
+    cur.end = true
+}
+
+
+func (this *WordDictionary) Search(word string) bool {
+    cur := this
+    for i := 0; i < len(word); i++ {
+        if word[i] == '.' {
+            for j := 0; j < 26; j++ {
+                if cur.letter[j] != nil {
+                    if cur.letter[j].Search(word[i+1:]) == true {
+                        return true
+                    }
+                }
+            }
+            return false
+        }
+        
+        if cur.letter[word[i]-'a'] == nil {
+            return false
+        }
+        cur = cur.letter[word[i] - 'a']
+    }
+    if cur.end == true {
+        return true
+    }
+    return false
+}
+
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AddWord(word);
+ * param_2 := obj.Search(word);
+ */
