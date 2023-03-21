60a1ee82680352d14a8c7fcd7a1998b1857a1de7
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-03-21 19:57:33 +0900

    Time: 147 ms (28.57%), Space: 9.8 MB (28.57%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.go
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.go
@@ -1,22 +1,13 @@
 func zeroFilledSubarray(nums []int) int64 {
     res := int64(0)
-    count := 0
+    count := int64(0)
     for i := 0; i < len(nums); i++ {
         if nums[i] == 0 {
             count++
-        }
-        if nums[i] != 0 && count != 0 {
-            res += countSubArray(count)
+        } else {
             count = 0
         }
-    }
-    if count != 0{
-        res += countSubArray(count)
+        res += count
     }
     return res
-}
-
-func countSubArray(count int) int64 {
-    res := count*(count + 1) / 2
-    return int64(res)
 }
