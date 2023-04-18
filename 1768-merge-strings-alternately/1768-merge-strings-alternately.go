ac71bb149c22ab0659807fb921e097942286fb8f
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2023-04-18 21:41:15 +0900

    Time: 0 ms (100.00%), Space: 2.7 MB (17.00%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.go
@@ -0,0 +1,13 @@
+func mergeAlternately(word1 string, word2 string) string {
+    res := ""
+    for i:= 0; i < len(word1) || i < len(word2); i++ {
+        if i < len(word1) {
+            res = res + string(word1[i])
+        }
+        
+        if i < len(word2) {
+            res = res + string(word2[i])
+        }
+    }  
+    return res
+}
