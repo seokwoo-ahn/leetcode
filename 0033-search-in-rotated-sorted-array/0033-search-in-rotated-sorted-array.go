bceca0426322c79669250c46a999bd9b837b0e7b
Cranberry <84396735+seokwoo-ahn@users.noreply.github.com>
2024-05-01 23:41:11 +0900

    Time: 0 ms (100.00%), Space: 2.7 MB (57.09%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -0,0 +1,32 @@
+func search(nums []int, target int) int {
+    start, end := 0, len(nums) -1
+    
+    for start <= end {
+        mid := (start + end) / 2
+        if nums[mid] == target {
+            return mid
+        }
+        
+        if nums[start] > nums[end] {
+            if target > nums[end] && target < nums[start] {
+                return -1
+            }
+        }
+            
+        if nums[start] <= nums[mid] {
+            if nums[start] <= target && target < nums[mid] {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[end] {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
+    }
+    
+    return -1
+}
